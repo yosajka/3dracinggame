4adaca43d86614cc93c724fc6dd6fd3625714ee9
Ken <nghiavdng108@gmail.com>
2022-11-20 05:37:42 +0700

    using RCC (realistic car controller)

--- Assets/Scripts/TimeManager/StartCoundown.cs
+++ Assets/Scripts/TimeManager/StartCoundown.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class StartCoundown : MonoBehaviour
 {
     public Text countDownDisplay;
+    public AudioSource countDownSound;
+    public AudioSource goSound;
 
     void Start()
     {
@@ -15,12 +17,16 @@ public class StartCoundown : MonoBehaviour
     IEnumerator CountDown()
     {
         countDownDisplay.text = "3";
+        countDownSound.Play();
         yield return new WaitForSeconds(1f);
         countDownDisplay.text = "2";
+        countDownSound.Play();
         yield return new WaitForSeconds(1f);
         countDownDisplay.text = "1";
+        countDownSound.Play();
         yield return new WaitForSeconds(1f);
         countDownDisplay.text = "GO!";
+        goSound.Play();
         yield return new WaitForSeconds(0.5f);
         countDownDisplay.enabled = false;
         LapTimeCounter.isCountingTime = true;
